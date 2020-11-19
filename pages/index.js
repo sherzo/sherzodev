a282181ae02c60674649f9b2eb06bd39ea78f561
Saul Florez <me@saulflorez.com>
2020-11-19 15:19:42 -0400

    📦 NEW: Inicializando el proyecto con Nextjs

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Index = () => {
+  return (
+    <div>
+      <h1>Sherzo.dev</h1>
+      <p>
+        Blog personal de <a href="https://github.com/sherzo">@sherzo</a>
+      </p>
+    </div>
+  );
+};
+
+export default Index;
