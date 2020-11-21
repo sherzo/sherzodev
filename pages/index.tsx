148eacbca018783d6ee06d059658336adb020b9f
Saul Florez <me@saulflorez.com>
2020-11-21 14:38:41 -0400

    📦 NEW: Agregando tailwindcss

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Index = () => {
   return (
-    <div>
-      <h1>Sherzo.dev</h1>
+    <div className="container">
+      <h1 className="text-5xl">Sherzo.dev</h1>
       <p>
         Blog personal de <a href="https://github.com/sherzo">@sherzo</a>
       </p>
