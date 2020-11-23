6eb59b9d187288af6bcf665dc3584e9edf6ac794
Saul Florez <me@saulflorez.com>
2020-11-23 15:11:26 -0400

    📦 NEW: Responsive del header home

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,22 +4,30 @@ import Image from "next/image";
 
 const IndexPage = () => {
   return (
-    <div>
-      <section className="bg-gradient-to-r from-blue-600  to-blue-900 px-4 pt-4">
-        <div className="max-w-3xl mx-auto">
-          <Navbar />
-          <div className="flex mt-2">
-            <p className="text-white text-3xl mt-20 leading-relaxed md:w-4/6">
-              Hi 👋, I'm Saul Florez{" "}
-              <a
-                className="text-yellow-300 font-mono font-semibold"
-                href="https://github.com/sherzo"
-                target="_blank"
-              >
-                @sherzo
-              </a>
-              . I'm Egineer and Software Developer
-            </p>
+    <section className="w-full bg-gradient-to-r from-blue-700 to-blue-900 px-4 pt-6">
+      <div className="mx-auto md:w-full lg:w-800">
+        <Navbar />
+        <div className="flex flex-col md:flex-row">
+          <div className="md:hidden my-6 rounded-full border self-center overflow-hidden bg-yellow-300 w-28 h-28">
+            <Image
+              src="/sherzo-big-photo.png"
+              alt="Sherzo Photo"
+              width="180"
+              height="180"
+            />
+          </div>
+          <p className="text-white text-3xl md:mt-20 leading-relaxed md:w-4/6 mb-5 lg:text-left text-center">
+            Hi 👋, I'm Saul Florez{" "}
+            <a
+              className="text-yellow-300 font-mono font-semibold"
+              href="https://github.com/sherzo"
+              target="_blank"
+            >
+              @sherzo
+            </a>
+            . I'm Egineer and Software Developer
+          </p>
+          <div className="md:block hidden mt-1 -mb-1.5 overflow-hidden">
             <Image
               src="/sherzo-big-photo.png"
               alt="Sherzo Photo"
@@ -28,8 +36,8 @@ const IndexPage = () => {
             />
           </div>
         </div>
-      </section>
-    </div>
+      </div>
+    </section>
   );
 };
 
