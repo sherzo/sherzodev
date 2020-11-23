8ca0f9f083b713a9c0a7da7fc97bb5e1d254eafe
Saul Florez <me@saulflorez.com>
2020-11-23 16:54:32 -0400

    📦 NEW: Extract components

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,31 +6,22 @@ const Navbar = () => {
     <nav className="flex md:w-full lg:w-800 absolute top-6 left-0 right-0 m-auto md:pl-0 pl-5">
       <div>
         <Link href="/">
-          <a className="text-white py-1 text-xl mr-24">Sherzo</a>
+          <a className="logo">Sherzo</a>
         </Link>
       </div>
       <ul className="md:flex items-center hidden">
         <li>
-          <a
-            href="#!"
-            className="text-white py-1 px-3 mx-6 hover:bg-blue-600 rounded transition duration-500"
-          >
+          <a href="#!" className="nav-item">
             Blog
           </a>
         </li>
         <li>
-          <a
-            href="#!"
-            className="text-white py-1 px-3 mx-6 hover:bg-blue-600 rounded transition duration-500"
-          >
+          <a href="#!" className="nav-item">
             About
           </a>
         </li>
         <li>
-          <a
-            href="#!"
-            className="text-white py-1 px-3 mx-6 hover:bg-blue-600 rounded transition duration-500"
-          >
+          <a href="#!" className="nav-item">
             Contact
           </a>
         </li>
